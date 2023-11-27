07c63318dc51dc819adeadc8bb2201c29d9e830c
Hubert Kłosowski <242424@edu.p.lodz.pl>
2023-11-27 17:28:37 +0100

    dziala haha

--- Zadanie3/website/predict.py
+++ Zadanie3/website/predict.py
@@ -1,22 +1,38 @@
-import numpy as np
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, request
 from .trainingModel import train_model, scale_data
 
-from .models import Iris, Species
-from . import db
+from .models import Iris
 
 predict = Blueprint('predict', __name__)
 
 
+def check_predict(iris):
+    for el in iris:
+        if not isinstance(el, float):
+            return 'All fields have to be numbers', False
+        if el <= 0:
+            return 'All fields have to be greater than 0', False
+    return 'Valid data', True
+
+
 @predict.route('/predict', methods=['GET'])
 def home():
-    iris = Iris.query.all()
-    X = [[i.sepal_length, i.sepal_width, i.petal_length, i.petal_width] for i in iris]
-    y = [i.species_id for i in iris]
-    train_model(X, y, [[5.1, 3.5, 1.4, 0.2]])
     return render_template("predict.html")
 
 
 @predict.route('/predict', methods=['POST'])
 def predict_iris():
-    pass
+    try:
+        iris = [float(request.form['sepal_length']), float(request.form['sepal_width']),
+                float(request.form['petal_length']), float(request.form['petal_width'])]
+    except ValueError as e:
+        print(e)
+        return render_template('400_error.html', error_message=str(e)), 400
+    check = check_predict(iris)
+    if check[1]:
+        test_iris = Iris.query.all()
+        X = [[i.sepal_length, i.sepal_width, i.petal_length, i.petal_width] for i in test_iris]
+        y = [i.species_id for i in test_iris]
+        return render_template("predict.html", result=train_model(X, y, [iris]))
+    else:
+        return render_template("predict.html", result=check[0])
