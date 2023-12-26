81b74caec251ff4070a83247b5364c8904329619
Hubert Kłosowski <242424@edu.p.lodz.pl>
2023-12-26 18:44:00 +0100

    poprawa klasyfikatora

--- Zadanie3/website/trainingModel.py
+++ Zadanie3/website/trainingModel.py
@@ -1,5 +1,7 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
+from .models import Iris
+from . import db
 
 
 def scale_data(X):
@@ -8,8 +10,17 @@ def scale_data(X):
     return scaler.transform(X)
 
 
+def count_species():
+    try:
+        species = db.session.query(Iris.species_id).distinct().all()
+        return len(species)
+    except Exception as e:
+        print(e)
+        return 0
+
+
 def train_model(x_train, y_train, x_test):
-    knn = KNeighborsClassifier(n_neighbors=3, weights='distance', metric='euclidean')
+    knn = KNeighborsClassifier(n_neighbors=count_species(), weights='distance', metric='euclidean')
     knn.fit(x_train, y_train)
     prediction = knn.predict(x_test)
     return prediction[0]
